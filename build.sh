9b97ddde40a53c0ac1318389e4745c651b748858
Dillon Winchell <dwinchel@redhat.com>
2022-02-10 17:56:13 -0500

    No more step runner

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ IMAGE_TAG=latest
 # Build pipeline bundles
 for YAML_FILE in $(ls resources/pipelines/*.yml); do
   TASK_NAME=$(basename ${YAML_FILE} | sed 's/\(.*\)\.yml/\1/')
-  tkn bundle push ${IMAGE_REGISTRY}/${IMAGE_REGISTRY_USER}/${TASK_NAME}-task-bundle:${IMAGE_TAG} -f ${YAML_FILE}
+  tkn bundle push ${IMAGE_REGISTRY}/${IMAGE_REGISTRY_USER}/${TASK_NAME}-bundle:${IMAGE_TAG} -f ${YAML_FILE}
 done
 
 # Build task bundles
