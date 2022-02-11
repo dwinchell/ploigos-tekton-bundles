fb922443e72728be0eef137603a9132a41ec5528
Dillon Winchell <dwinchel@redhat.com>
2022-02-11 07:34:40 -0500

    Download and document upstream tasks from Tekton Catalog

--- install-third-party.sh
+++ install-third-party.sh
@@ -1,3 +1,3 @@
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/maven/0.2/maven.yaml
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/sonarqube-scanner/0.1/sonarqube-scanner.yaml
-
+for f in $(ls third-party/*.yaml); do
+  oc apply -f $f
+done
