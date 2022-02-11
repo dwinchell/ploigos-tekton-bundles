ac024dc1459da4851a4b12522b679db588829757
Dillon Winchell <dwinchel@redhat.com>
2022-02-11 05:55:49 -0500

    Add dev mode

--- install-dev.sh
+++ install-dev.sh
@@ -6,9 +6,8 @@ IMAGE_REGISTRY_USER=dwinchell_redhat
 IMAGE_TAG=latest
 
 # Build pipeline bundles
-YAML_FILE=resources/pipelines/ploigos-everything-pipeline.yml
-TASK_NAME=$(basename ${YAML_FILE} | sed 's/\(.*\)\.yml/\1/')
-oc apply -f ${YAML_FILE}
+oc delete pipeline ploigos-everything-pipeline
+oc create -f resources/pipelines/ploigos-everything-pipeline.yml
 
 # Build task bundles
 for YAML_FILE in $(ls resources/tasks/*.yml); do
